aafa4f838065cc8d784095dd192e80eb7d5b16df
residentevil0803 <ramseyklassen0803@gmail.com>
2019-08-27 19:22:02 -0500

    deleting src on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" />
     <HelloWorld msg="Welcome to the Store" />
   </div>
 </template>
