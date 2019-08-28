852d9c2a51f10c12e8dd8818e67f2189c3165aa8
residentevil0803 <ramseyklassen0803@gmail.com>
2019-08-27 19:38:43 -0500

    adding horizontal barrier in header;

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/products">Products</router-link>
+      <router-link to="/products">Products</router-link> |
       <router-link to="/cart">Shopping Cart</router-link>
     </div>
     <router-view />
