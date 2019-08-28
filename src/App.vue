24819e7a8a747532f55210aa83d846e7715c7fea
residentevil0803 <ramseyklassen0803@gmail.com>
2019-08-27 19:37:25 -0500

    linking shopping cart

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/products">Products</router-link>
+      <router-link to="/cart">Shopping Cart</router-link>
     </div>
     <router-view />
   </div>
