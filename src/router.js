435210c8ae73a8a8e7e862078fbe69caf6955cfc
residentevil0803 <ramseyklassen0803@gmail.com>
2019-08-27 19:34:28 -0500

    adding products vue to router

--- src/router.js
+++ src/router.js
@@ -16,6 +16,12 @@ export default new Router({
       name: "about",
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
 });
