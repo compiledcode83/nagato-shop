837373ee645434b88c52640fadb9b880110e435f
residentevil0803 <ramseyklassen0803@gmail.com>
2019-08-28 20:48:07 -0500

    commenting out div nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/products">Products</router-link> |
+      <router-link to="/cart">Shopping Cart</router-link>
+    </div> -->
     <router-view />
   </div>
 </template>
