261f628ee8a9a17b53b9039dc5efdd6e2a90b531
residentevil0803 <ramseyklassen0803@gmail.com>
2019-08-28 20:47:15 -0500

    cleaning up code

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -2,9 +2,9 @@
 // add search option - bootstrap
 <template>
   <div class="header" id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/products">Products</router-link> |
-      <router-link to="/cart">Shopping Cart</router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/products">Products</router-link> |
+    <router-link to="/cart">Shopping Cart</router-link>
   </div>
-</template>
+</template>Î
