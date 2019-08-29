baaaba839a3027cdcbbd837fe119c3dc6cf3b470
residentevil0803 <ramseyklassen0803@gmail.com>
2019-08-28 20:46:54 -0500

    adding router links to header view

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,6 +1,10 @@
 // header here
 // add search option - bootstrap
 <template>
-  <div class="header">
+  <div class="header" id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/products">Products</router-link> |
+      <router-link to="/cart">Shopping Cart</router-link>
   </div>
 </template>
